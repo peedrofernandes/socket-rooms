c3c692646f2cadc9235198b998ac5e15ce175efa
Pedro Fernandes <gm.pedro.fernandes@gmail.com>
2022-10-09 18:13:24 -0300

    Added domain entities

---
+++ src/domain/entities.ts
@@ -0,0 +1,29 @@
+export type User = {
+  fullName: string
+  alias: string
+  email: string
+  createdAt: Date
+}
+
+export type Chat = {
+  user1: User
+  user2: User
+  messages: Message[]
+  createdAt: Date
+}
+
+export type Message = {
+  sender: User
+  receiver: User
+  chat: Chat
+  sentAt: Date
+  receivedAt: Date
+  seenAt: Date
+}
+
+export type GlobalNetwork = {
+  totalUsers: User[]
+  totalChats: Chat[]
+  totalMessages: Message[]
+  usersOnline: User[]
+}
