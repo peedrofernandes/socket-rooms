94afe720590d9b95ea50b4c79dc7beca066b94f6
Pedro Fernandes <gm.pedro.fernandes@gmail.com>
2022-10-10 14:05:10 -0300

    Added application usecases

---
+++ src/application/usecases.ts
@@ -0,0 +1,84 @@
+import { Chat, Message, User } from "../domain/entities"
+import { ChatRepository, MessageRepository, UserRepository } from "../domain/repositories"
+import { MarkAsReceived, MarkAsSent, ReceiveOwnChats, ReceivePendingMessages, ReceiveUserList, VerifyExistingChat, VerifyUserAuthorization } from "../domain/rules"
+
+
+type ConnectToNetworkUseCase = (params: {
+  user: User
+  connectionDate: Date
+  authorizationParams: {
+    credentials: any
+    verifyAuthorizationMethod: (user: User, credentials: any) => Promise<boolean> | boolean
+  }
+  messageRepository: MessageRepository
+  chatRepository: ChatRepository
+  userRepository: UserRepository
+}) => Promise<{
+  chats: Chat[]
+  messages: Message[]
+  users: User[]
+}>
+export const ConnectToNetwork: ConnectToNetworkUseCase = async (params) => {
+  const {
+    user,
+    connectionDate,
+    authorizationParams,
+    messageRepository,
+    chatRepository,
+    userRepository
+  } = params
+
+  VerifyUserAuthorization({ user, ...authorizationParams })
+  const chats = await ReceiveOwnChats({ user, chatRepository })
+  const messages = await ReceivePendingMessages({ user, chats, messageRepository })
+  await MarkAsReceived({ data: messages, date: connectionDate, messageRepository })
+  const users = await ReceiveUserList({ user, userRepository })
+
+  return { chats, messages, users }
+}
+
+
+type StartChatUseCase = (params: {
+  starter: User
+  target: User
+  chatRepository: ChatRepository
+}) => Promise<Chat>
+export const StartChat: StartChatUseCase = async (params) => {
+  const { starter, target, chatRepository } = params
+
+  VerifyExistingChat({ user1: starter, user2: target, chatRepository })
+
+  const newChat: Chat = {
+    id: await chatRepository.generateId(),
+    createdAt: new Date(),
+    messages: new Array<Message>,
+    user1: starter,
+    user2: target
+  }
+
+  await chatRepository.create(newChat)
+  return newChat
+}
+
+
+type SendMessageUseCase = (params: {
+  sender: User
+  receiver: User
+  chat: Chat
+  messageRepository: MessageRepository
+  chatRepository: ChatRepository
+}) => Promise<void>
+
+export const SendMessage: SendMessageUseCase = async (params) => {
+  const { sender, receiver, chat, messageRepository, chatRepository } = params
+
+  const newMessage: Message = {
+    id: await messageRepository.generateId(),
+    sender,
+    receiver,
+    chat
+  }
+
+  await messageRepository.create(newMessage)
+  await MarkAsSent({ data: newMessage, date: new Date(), messageRepository })
+}
