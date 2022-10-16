279601580b71363e67007ec584705b8175acec74
Pedro Fernandes <gm.pedro.fernandes@gmail.com>
2022-10-16 13:29:40 -0300

    Added main entities

---
+++ src/domain/entities.ts
@@ -0,0 +1,25 @@
+export type User = {
+  id: string
+  fullName: string
+  email: string
+  createdAt: Date
+}
+
+export type Chat = {
+  id: string
+  user1: User
+  user2: User
+  messages: Message[]
+  qtyMessages: number
+  createdAt: Date
+}
+
+export type Message = {
+  id: string
+  sender: User
+  receiver: User
+  chat: Chat
+  sentAt: Date
+  receivedAt: Date
+  seenAt: Date
+}
